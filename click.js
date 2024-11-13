9d54de101decc5f39b49931f5b272d3337e3db3a
WizardTop1 <norhelarot@gmail.com>
2024-11-13 16:33:39 +0400

    Add files via upload

---
+++ click.js
@@ -0,0 +1,7 @@
+document.getElementById("myButton").onclick = function() {
+    window.location.href = "https://forms.gle/7GZzFC7NoPwMYcUk6";
+};
+
+document.getElementById("myButton").onclick = function() {
+    window.location.href = "https://forms.gle/Qt78hxjkLsHSLVT69";
+};
